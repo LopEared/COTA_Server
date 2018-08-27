12bdc2564e524674f7387a873d4a1ab290c0fa86
Михаил Микулич <mikulich.m@belintersat.by>
2018-08-28 01:35:42 +0300

    123

--- js/_toServer.js
+++ js/_toServer.js
@@ -1,5 +1,28 @@
 // Здесь описывается скрипт работы с сервером: прямая/обратная конвертцация обьектов в JSON-формат и отправка/запрос строки JSON
+var textbox;
+function MakeJSON() {
+	console.log("Привет!");
+	textbox = JSON.stringify(sat); // тут запихиваем нашу JSON-строку
+	console.log(textbox);
+}
 
+/*
+function objectSaver() {
+	var textFile = null,
+	makeTextFile = function(text) {
+			var data = new Blob([text], {type: 'text/plain'});
+			// If we are replacing a previously generated file we need to
+			// manually revoke the object URL to avoid memory leaks.
+			if (textFile !== null) {window.URL.revokeObjectURL(textFile);}
+			textFile = window.URL.createObjectURL(data);
+			return textFile;
+		};
+	var create = document.getElementById('saveObjectServer'),
+	create.addEventListener('click',MakeLink(), false);
+	};
+
+ function MakeLink() {var link = document.getElementById('saveLocalMachineButton'); link.href = makeTextFile(textbox.value);}
+/*
 // 1. Создаём новый объект XMLHttpRequest
 var xhr = new XMLHttpRequest();
 // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
@@ -14,10 +37,6 @@ if (xhr.status != 200) {
   // вывести результат
   alert( xhr.responseText ); // responseText -- текст ответа.
 }
-
-
-
-
 function objectSendedServer(){
-	
 }
+*/
