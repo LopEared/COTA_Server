c1fb81fcecaa759175a7c3ff13403ed7e10b8f63
LopEared <40773335+LopEared@users.noreply.github.com>
2019-01-27 19:44:54 +0300

    PHP_START

--- hello.php
+++ hello.php
@@ -1,10 +1,22 @@
-<?php
+<?php 
+date_default_timezone_set('Europe/Minsk');
+echo '<p>Привет, мир!</p>';
 
-// Показывать всю информацию, по умолчанию INFO_ALL
-phpinfo();
+echo date("Y-m-d_G:i:s");
 
-// Показывать информацию только о загруженных модулях.
-// phpinfo(8) выдает тот же результат.
-phpinfo(INFO_MODULES);
+$date_name=date("Y-m-d_G-i-s");
+echo "<p>Date right now: $date_name </p>";
 
+$file_name="$date_name.txt";
+$file_text="Привет, Михаил!";
+	if(!file_exists($file_name)){
+		echo "Создаем файл $file_name";
+		file_put_contents($file_name,$file_text);
+	}
+	else{
+		echo 'Файл существует! <br>';
+		echo 'Перезаписываем файл! <br>';
+		file_put_contents($file_name,$file_text,file_append);
+	}
+#phpinfo();
 ?>
